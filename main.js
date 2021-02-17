efededaf96fd515cadeade959e236e65d53d4352
jux17 <jeffreyux06@gmail.com>
2021-02-17 15:45:25 -0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-$('#chevron').click(function() {
-	$('#mainSite').show();
-	$.scrollTo('#mainSite',{
-		duration: 700
-		})
-})
-$('#mtb').on('mouseover', function() {
-	$('#mtb').toggleClass('animated rubberBand');
-})
